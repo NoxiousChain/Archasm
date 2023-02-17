6fa9dafd24bfb00948ec774fcc7b1399ce329224
jcj76 <joelcjohnson22@gmail.com>
2023-02-16 21:48:46 -0500

    added movement and player model as well as picaxe

--- godotproject/CameraControl.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-onready var player = get_node("/root/Node2D/Player")
-
-func _process(delta):
-	position.x = player.position.x
--- godotproject/player.gd
+++ godotproject/player.gd
@@ -1,24 +1,80 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+export var gravity = 500
+export var acc = 400
+export var de_acc = 150
+export var max_move_speed = 100
+export var jump_height = 200
 
-func _ready():
-	screen_size = get_viewport_rect().size
+var vspeed : float = 0
+var hspeed : float = 0
+var move_speed : float = 0
 
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_jump"):
-		velocity.y -= 1
+var touching_ground : bool = false
+var coy_time : bool = false # this will let us do coytoee time
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
+var motion : Vector2 = Vector2.ZERO
+var up : Vector2 = Vector2.UP
+
+onready var coy_timer = $coyote_timer
+
+func _physics_process(delta: float) -> void:
+	quick_move(delta)
+	apply_physics(delta)
+
+func apply_physics(delta : float) -> void:
+
+	if(is_on_ceiling()):
+		motion.y = 10
+		vspeed = 10
+	
+	if(touching_ground == true and !is_on_floor()): # just left the ground
+		coy_time = true
+		coy_timer.start(0.2)
+		
+	if(is_on_floor()):
+		touching_ground = true
+		coy_timer.stop()
+		coy_time = false
 	else:
-		$AnimatedSprite.stop()
+		touching_ground = false
+
+	if(!is_on_floor()):
+		vspeed += (gravity * delta)
+	else:
+		vspeed = 0
+		if(Input.is_action_just_pressed("move_jump")):
+			coy_time = false
+			coy_timer.stop()
+			vspeed = -jump_height
+			
+	if(coy_time and Input.is_action_just_pressed("move_jump")):
+		coy_time = false
+		coy_timer.stop()
+		vspeed = -jump_height
+
+	motion.y = vspeed
+	motion.x = hspeed
+
+	motion = move_and_slide(motion,up)
+
+
+func quick_move(var delta : float) -> void:
+
+	if(is_on_wall()):
+		move_speed = 0
+
+	if(Input.is_action_pressed("move_right")):
+		move_speed += acc * delta
+	elif(Input.is_action_pressed("move_left")):
+		move_speed -= acc * delta
+	else:
+		move_speed = lerp(move_speed,0,0.5)
+		
+	move_speed = clamp(move_speed,-max_move_speed,max_move_speed)
+	
+	hspeed = move_speed
+
+
+func _on_coyatoe_timer_timeout() -> void:
+	coy_time = false
