441385e72576cf06c7002cd31c4eb320db931cab
kps78 <kps78@drexel.edu>
2023-02-27 23:13:17 -0500

    main menu additions

--- godotproject/Menu.gd
+++ godotproject/Menu.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/Startbutton.grab_focus()
 
 func _on_Startbutton_pressed():
 	get_tree().change_scene("res://World.tscn")
