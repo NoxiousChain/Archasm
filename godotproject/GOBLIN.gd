e667b4b2c1aa71daf2e1f69b27c97f75d34946d1
Jack Thompson <jmt454@drexel.edu>
2023-03-10 15:11:55 -0500

    enemies randomly spawn, movement not completed

--- godotproject/GOBLIN.gd
+++ godotproject/GOBLIN.gd
@@ -1,76 +1,48 @@
 extends KinematicBody2D
 
-var speed = 200
+var speed = 20000
 var motion : Vector2
-var up : Vector2
-var gravity = 600
+var gravity = 200
 var move_speed : float = 0
 export var max_move_speed = 250
 
 var vspeed : float = 0
-var hspeed : float = 0
+var hspeed : float = 50
+
+#var screen_size = get_viewport().get_visible_rect().size
+var screen_size = get_viewport_rect().size
 
 onready var sprite : Sprite = get_node("Sprite")
 
 var touching_ground : bool = false
-var direction = 1 #1 = right, -1 = left
-
-var coy_time : bool = false
-onready var coy_timer = $coyote_timer
-
+var direction = 1 # 1 = right, -1 = left
 
 func _physics_process(delta: float) -> void:
-	quick_move(delta)
+	movement(delta)
 	apply_physics(delta)
 
+
 func apply_physics(delta : float) -> void:
-
-	if(is_on_ceiling()):
-		motion.y = 10
-		vspeed = 10
-	
-	if(touching_ground == true and !is_on_floor()): # just left the ground
-		coy_time = true
-		coy_timer.start(0.2)
-		
-	if(is_on_floor()):
-		touching_ground = true
-		coy_timer.stop()
-		coy_time = false
-	else:
-		touching_ground = false
-
-	if(!is_on_floor()):
-		vspeed += (gravity * delta)
-	else:
-		vspeed = 0
+	vspeed += (gravity * delta)
 	motion.y = vspeed
 	motion.x = hspeed
+	
+	motion = move_and_slide(motion)
 
-	motion = move_and_slide(motion,up)
 
-
-func quick_move(var delta : float) -> void:
-
-	if(is_on_wall()):
-		move_speed = 0
+func movement(var delta : float) -> void:
+	
+	if position.x < screen_size.x:    
+		direction = 1
+	if position.x > screen_size.x:
+		direction = -1
 
 	if(direction == 1):
 		move_speed += speed * delta
-		if(direction == -1):
-			move_speed = 0
 	elif(direction == -1):
 		move_speed -= speed * delta
-		if(direction == 1):
-			move_speed = 0
-	else:
-		move_speed = lerp(move_speed,0,0.5)
 		
 	move_speed = clamp(move_speed,-max_move_speed,max_move_speed)
 	
-	hspeed = move_speed
-	if motion.x < 0:
-		sprite.flip_h = true
-	if motion.x > 0:
-		sprite.flip_h = false
+	
 
--- godotproject/World.gd
+++ godotproject/World.gd
@@ -4,3 +4,16 @@ extends Node2D
 func _ready():
 	$AnimationPlayer.play("DayNight_cycle")
 	get_node("ParallaxBackground/AnimationPlayer").play("DayNightBack_cycle")
+
+	var rand = RandomNumberGenerator.new()
+	var enemyscene = load("res://GOBLIN.tscn")
+	
+	var screen_size = get_viewport().get_visible_rect().size
+	
+	for i in range(0,3):
+		var enemy = enemyscene.instance()
+		rand.randomize()
+		var x = rand.randf_range(0, screen_size.x)
+		enemy.position.y = -screen_size.y/2
+		enemy.position.x = x
+		add_child(enemy)
