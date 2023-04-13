96d7e3389ccd20c933bec9db9b75539828c88c5a
NoxiousChain <kevco907@gmail.com>
2023-04-13 15:16:41 -0400

    pause menu functions

--- godotproject/scripts/GamePause.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_Resumegme_pressed():
-	var _err = get_tree().change_scene("res://scenes/World.tscn")
-
-func _on_MMbutton_pressed():
-	var _err = get_tree().change_scene("res://scenes/Menu.tscn")
---
+++ godotproject/scripts/PauseCanvas.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+onready var continueButton = $Continue
+onready var quitButton = $MainM
+onready var texRect = $TextureRect
+func _ready() -> void:
+	quitButton.visible = false
+	continueButton.visible = false
+	texRect.visible = false
+	pass
+
+func _input(event: InputEvent) -> void:
+	
+	if event.is_action_pressed("paused"):
+		get_tree().paused = !get_tree().paused
+		quitButton.visible = true
+		continueButton.visible = true
+		texRect.visible = true
+
+	pass
+
+
+func _on_Continue_pressed() -> void:
+	get_tree().paused = false
+	quitButton.visible = false
+	continueButton.visible = false
+	texRect.visible = false
+	pass # Replace with function body.
+
+
+func _on_MainM_pressed():
+	quitButton.visible = false
+	continueButton.visible = false
+	texRect.visible = false
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/Menu.tscn")
+	pass # Replace with function body.
