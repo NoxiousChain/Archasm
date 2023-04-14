61f76a2acf76c41c085bbcbd5995231470c21a64
NoxiousChain <kevco907@gmail.com>
2023-04-14 18:17:50 -0400

    design overhaul of buttons and text

--- godotproject/scripts/PauseCanvas.gd
+++ godotproject/scripts/PauseCanvas.gd
@@ -3,21 +3,22 @@ extends CanvasLayer
 onready var continueButton = $Continue
 onready var quitButton = $MainM
 onready var texRect = $TextureRect
+onready var pLabel = $PauseLabel
 func _ready() -> void:
 	quitButton.visible = false
 	continueButton.visible = false
 	texRect.visible = false
+	pLabel.visible= false
 	pass
 
 func _input(event: InputEvent) -> void:
-	
-	if event.is_action_pressed("paused"):
+	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = !get_tree().paused
 		quitButton.visible = true
 		continueButton.visible = true
 		texRect.visible = true
-
-	pass
+		pLabel.visible = true
+		pass
 
 
 func _on_Continue_pressed() -> void:
@@ -25,6 +26,7 @@ func _on_Continue_pressed() -> void:
 	quitButton.visible = false
 	continueButton.visible = false
 	texRect.visible = false
+	pLabel.visible= false
 	pass # Replace with function body.
 
 
@@ -32,6 +34,7 @@ func _on_MainM_pressed():
 	quitButton.visible = false
 	continueButton.visible = false
 	texRect.visible = false
+	pLabel.visible= false
 	get_tree().paused = false
 	get_tree().change_scene("res://scenes/Menu.tscn")
 	pass # Replace with function body.
