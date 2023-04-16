3e5efd96b189db6322bcc56fd4d0dd7c391604e1
NoxiousChain <kevco907@gmail.com>
2023-04-16 17:56:25 -0400

    fixed bugs in recent commit

--- godotproject/scripts/PauseCanvas.gd
+++ godotproject/scripts/PauseCanvas.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	pass
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("paused"):
 		get_tree().paused = !get_tree().paused
 		quitButton.visible = true
 		continueButton.visible = true
