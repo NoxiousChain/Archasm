2b839a70bfad8c94af366488436d650467be835b
Jack Thompson <jmt454@drexel.edu>
2023-04-24 11:36:07 -0400

    added npc guide with mini tutorial

---
+++ godotproject/scenes/NPC.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var active = false
+
+func _ready():
+	connect("body_entered", self, '_on_NPC_body_entered')
+	connect("body_exited", self, '_on_NPC_body_exited')
+
+func _input(event): #press enter when in range of the NPC to start dialog.
+	if get_node_or_null('DialogNode') == null:
+		if event.is_action_pressed("ui_accept") and active:
+			get_tree().paused = true
+			var dialog = Dialogic.start('start-game-dialog')
+			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
+			dialog.connect('timeline_end', self, 'unpause')
+			add_child(dialog)
+
+func _on_NPC_body_entered(body):
+	if body.name == "player":
+		active = true
+		
+func _on_NPC_body_exited(body):
+	if body.name == 'player':
+		active = false
