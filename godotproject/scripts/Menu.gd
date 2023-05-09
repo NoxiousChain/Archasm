87ac470e95637a979e463485ef815fd0a3d17edf
Jack Thompson <jmt454@drexel.edu>
2023-05-08 20:30:26 -0400

    fixed crashing bug with music

--- godotproject/scripts/Menu.gd
+++ godotproject/scripts/Menu.gd
@@ -1,10 +1,14 @@
+
 extends Control
+
+onready var menu_music = get_node("Music")
 
 func _ready():
 	pass # Replace with function body.
 
 func _on_Startbutton_pressed():
-	$Music.stop()
+	if menu_music != null:
+		menu_music.stop()
 	var _err = get_tree().change_scene("res://scenes/World.tscn")
 
 func _on_Quit_pressed():
@@ -12,3 +16,5 @@ func _on_Quit_pressed():
 	
 func _on_Options_pressed():
 	var _err = get_tree().change_scene("res://scenes/ControlsMenu.tscn")
+	
+	
