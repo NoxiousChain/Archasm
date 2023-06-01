492682fe5d6ae9cc2d7a2e8fe82c9507b0c1de06
NoxiousChain <kevco907@gmail.com>
2023-06-01 11:12:29 -0400

    new intro added

--- godotproject/scripts/Intro.gd
+++ godotproject/scripts/Intro.gd
@@ -4,8 +4,6 @@ func _ready():
 	MusicControls.play_music()
 	$AnimationPlayer.play("Fadein")
 	yield(get_tree().create_timer(6), "timeout")
-	$AnimationPlayer.play("Fadeout")
-	yield(get_tree().create_timer(3), "timeout")
 	var _err = get_tree().change_scene("res://scenes/Menu.tscn")
 
 func _input(_event):
